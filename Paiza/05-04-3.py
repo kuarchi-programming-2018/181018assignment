f707f00f5e038e44e5aedd54a4bb3e731a274e0c
HaraKento <43873802+HaraKento@users.noreply.github.com>
2018-10-12 23:45:40 +0900

    after edit

---
+++ Paiza/05-04-3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+points = {"国語" : 70, "算数" : 35, "英語" : 52}
+sum = 0
+# この下で、辞書の値の合計をループで計算してみよう
+for i in points:
+    sum=sum+points[i]
+print(int(sum))
+
+    
+
+    
+
+
+
+ 
+    
